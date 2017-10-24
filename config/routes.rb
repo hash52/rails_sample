36d359bc28ebfa4177539fec105b709abbb94dd5
ULM Co <ULM@Takayuki-no-iMac.local>
2017-10-24 19:37:57 +0900

    config named root

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'static_pages/contact'
+  root 'static_pages#home'
+#  get 'static_pages/home' 今後は常にroot_path,root_urlを使っていく
+
+#  get 'static_pages/help'
+#  static_pages/helpでアクセスするのは回りくどいので、
+#  /helpでstatic_pages#helpにアクセスしたい
+  get '/help',to:'static_pages#help'#,as:'helf' ->/helpでアクセスでき、helf_pathでパスを取得できる
+  get '/about',to:'static_pages#about'
+  get '/contact',to:'static_pages#contact'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,33 +5,30 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
-  test "should get root" do
-    get root_url
-    assert_response :success
-  end
 
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title","Home | #{@base_title}"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    #route.rbの変更により名前付きルート(*_path)が使えるようになった
+    get help_path
     assert_response :success
     assert_select "title","Help | #{@base_title}"
   end
 
   test "should get about" do
     #routes.rbを見る
-    get static_pages_about_url
+    get about_path
     assert_response :success
     #指定のタグに指定の文字列があるかどうかチェックするテストメソッド(セレクタ)
     assert_select "title","About | #{@base_title}"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title","Contact | #{@base_title}"
   end
