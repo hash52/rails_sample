2249ba28bd5aff55cd4ea769c84f924ce365689c
ULM Co <ULM@Takayuki-no-iMac.local>
2017-10-24 20:36:55 +0900

    add User. Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
-#  get 'static_pages/home' 今後は常にroot_path,root_urlを使っていく
 
 #  get 'static_pages/help'
 #  static_pages/helpでアクセスするのは回りくどいので、
@@ -9,4 +8,7 @@ Rails.application.routes.draw do
   get '/help',to:'static_pages#help'#,as:'helf' ->/helpでアクセスでき、helf_pathでパスを取得できる
   get '/about',to:'static_pages#about'
   get '/contact',to:'static_pages#contact'
+  get '/signup',to:'users#new'
+# get 'users/new',as:'signup' /users/new で users#new にアクセスできて、コード内ではsignup_pathでパスを扱える
+
 end
