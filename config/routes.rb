c35a9bd2e1808b5dd6d8ae9091573b8de6be68a6
ULM Co <ULM@Takayuki-no-iMac.local>
2017-10-25 11:31:12 +0900

    remove unnecessary comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
-
-#  get 'static_pages/help'
-#  static_pages/helpでアクセスするのは回りくどいので、
-#  /helpでstatic_pages#helpにアクセスしたい
   get '/help',to:'static_pages#help'#,as:'helf' ->/helpでアクセスでき、helf_pathでパスを取得できる
   get '/about',to:'static_pages#about'
   get '/contact',to:'static_pages#contact'
