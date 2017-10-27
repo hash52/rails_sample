97b327ac4675b9dd1f0218027b8763a42b28a9f7
ULM Co <ULM@Takayuki-no-iMac.local>
2017-10-27 13:14:41 +0900

    add sessions#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   get '/help',to:'static_pages#help'#,as:'helf' ->/helpでアクセスでき、helf_pathでパスを取得できる
@@ -8,6 +9,11 @@ Rails.application.routes.draw do
   get '/signup',to:'users#new'
   post '/signup', to:'users#create'
   # get 'users/new',as:'signup' /users/new で users#new にアクセスできて、コード内ではsignup_pathでパスを扱える
+
+  # SessionをRESTfulに扱う。リソースをDBではなくcookieで管理する。
+  get '/login', to:'sessions#new'
+  post '/login', to:'sessions#create'
+  delete '/logout', to:'sessions#destroy'
 
   #RESTfulなUsersリソースで必要となる全てのアクションが利用できるようになる
   resources :users
