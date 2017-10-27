92fe38946f053b3a5bcc07c8732d1bf83bc5d550
ULM Co <ULM@Takayuki-no-iMac.local>
2017-10-27 13:40:01 +0900

    add authencation but contains bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    # authenticate ・・ has_secure_passwordが提供するメソッド
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      # エラーメッセージを作成する(sessionはActiveRecodのモデルではないのでerrorsオブジェクトを持っていない)
+      flash[:danger] = 'Invalid email/password combination'
+      # bug! renderでテンプレートを強制的に再レンダリングしてもリクエストとみなされないため、flashメッセージが残り続けてしまう
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
