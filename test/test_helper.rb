e44867a8e5c0a3ec4b55dbdedbfaa42c95b38ce3
ULM Co <ULM@Takayuki-no-iMac.local>
2017-10-30 20:46:30 +0900

    add remember_me test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,4 +15,21 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !session[:user_id].nil?
   end
+
+  # sessionハッシュとpostを使わずに、テストユーザとしてログインする
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  # 統合テストで扱うヘルパー
+
+  # テストユーザーとしてログインする
+  # 単体テストか統合テストか意識せずに、ログイン済みの状態をテストしたい時はlog_in_asを呼び出せばいい＝ダックタイピングの一種
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
 end
