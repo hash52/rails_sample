8904083893082ee5c746dec7a2768bc7285736b6
ULM Co <ULM@Takayuki-no-iMac.local>
2017-10-30 18:34:04 +0900

    add remember user logic

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,11 +7,28 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # ユーザのセッションを永続的にする
+  def remember user
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    # cookies[:remember_token] = { value:   remember_token,expires: 20.years.from_now.utc } と等価
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   #現在ログイン中のユーザを返す
   def current_user
-    # ||= or equal @current_user = @current_user || User.find_by(id:session[:user_id])と等価
-    # @current_userがnilではなければ(存在していれば)、find_byは評価されない
-    @current_user ||= User.find_by(id: session[:user_id])
+    # 代入した結果nilじゃなければ
+    if (user_id = session[:user_id])
+      # ||= or equal @current_user = @current_user || User.find_by(id:session[:user_id])と等価
+      # @current_userがnilではなければ(存在していれば)、find_byは評価されない
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   def logged_in?
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,11 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
+  # 渡されたトークンがダイジェストと一致したらtrue
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
   #　クラスメソッドをまとめて記述できる
   class << self
     # 渡された文字列のハッシュ値を返す
